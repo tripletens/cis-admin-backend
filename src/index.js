d31eea34ca6e9cad4f133e12a775cb9862bf093e
Kalu Chinonso <tripletens.kc@gmail.com>
2023-05-26 10:26:26 +0100

    add cors origin *

--- src/index.js
+++ src/index.js
@@ -1,47 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRouter = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const connectDB = require('./config/db');
-const authRouter = require('./routes/auth');
-const cors = require('cors');
 
-// connect to database 
+// Connect to the database
 connectDB();
 
 app.use(express.json());
 
-// // cors 
-app.use(cors(
-  {
-    origin: 'http://localhost:3000/',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    headers: {
-      'Content-Type': 'application/json',
-      // Add other headers if needed
-    }
-  }
-));
+// CORS configuration
+app.use(cors());
 
 // Mount the auth router
 app.use('/auth', authRouter);
 
-// result const 
-let result = {
-  status : null,
-  message : null,
-  data : null,
-  token : null
-};
-
-// other code for setting up the express app goes here
+// Other code for setting up the Express app goes here
 
 app.listen(port, () => {
   console.log(`App is currently running on port https://localhost:${port}`);
 });
 
-// add routes
+// Add routes
 app.get("/", (req, res) => {
-  res.send("landing page for cis ");
+  res.send("Landing page for CIS");
 });
