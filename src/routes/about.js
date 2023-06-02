c55ff840c494343b8315b85fe8ac598be6a87ec0
Kalu Chinonso <tripletens.kc@gmail.com>
2023-06-02 21:57:56 +0100

    add the about our identity section

--- src/routes/about.js
+++ src/routes/about.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const aboutOurImpactController = require('../controllers/about/aboutOurImpactController');
 const aboutFoundersVisionController = require('../controllers/about/aboutFoundersVisionController');
+const aboutOurIdentityController = require('../controllers/about/aboutOurIdentityController');
 
 // Define routes
 router.post('/founder-vision/update', aboutFoundersVisionController.update);
