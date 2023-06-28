8c46f41317277798d471e755a229dfc98489e2d3
Kalu Chinonso <tripletens.kc@gmail.com>
2023-06-29 00:37:50 +0100

    rewrite the auth functions

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,17 +3,7 @@ const jwt = require("jsonwebtoken");
 const AdminUser = require("../models/admins");
 require("dotenv").config();
 
-// result const Nonny4real2023
-
-let result = {
-    status : null,
-    message : null,
-    data : null,
-    token : null
-  };
-
 // Register new user
-
 exports.register = async (req, res) => {
   try {
     const { firstname, lastname, username, email, password } = req.body;
@@ -21,14 +11,14 @@ exports.register = async (req, res) => {
     // Check if the user already exists
     const adminUser = await AdminUser.findOne({ email }).lean();
     if (adminUser) {
-      result.message = "Admin with email already exists";
-      result.status = false;
-      return res.status(400).json(result);
+      return res.status(400).json({
+        status: false,
+        message: "Admin with email already exists",
+      });
     }
 
     // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create new user
     const newAdminUser = new AdminUser({
@@ -45,57 +35,21 @@ exports.register = async (req, res) => {
     // Remove the password property from the response
     const { password: pw, ...admin } = savedAdmin.toObject();
 
-    result.message = "Admin Account created successfully";
-    result.data = admin;
-
-    res.status(201).json(result);
+    return res.status(201).json({
+      status: true,
+      message: "Admin account created successfully",
+      data: admin,
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error(error);
+    return res.status(500).json({
+      status: false,
+      message: "An error occurred during registration",
+    });
   }
 };
 
 // Login user
-// exports.login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Check if the user exists
-//     const adminUser = await AdminUser.findOne({ email }, { maxTimeMS: 100000 });
-//     if (!adminUser) {
-//         result.message = "Invalid credentials";
-//         result.status = false;
-//       return res.status(400).json(result);
-//     }
-
-//     // Check if the password is correct
-//     const validPassword = await bcrypt.compare(password, adminUser.password);
-//     if (!validPassword) {
-//         result.message = "Invalid credentials";
-//         result.status = false;
-//       return res.status(400).json(result);
-//     }
-
-//     // Create and sign a token
-//     const token = jwt.sign({ _id: adminUser._id }, process.env.JWT_SECRET, {
-//       expiresIn: "1h",
-//     });
-
-//     // Remove the password property from the response
-//     const { password: pw, ...data } = adminUser.toObject();
-
-//     result.message = "Admin Logged in successfully";
-//     result.data = data;
-//     result.token = token;
-//     result.status = true;
-
-//     res.status(200).json(result);
-//   } catch (error) {
-//     result.message = error.message + " hello error here ";
-//     result.status = false;
-//     res.status(500).json(result);
-//   }
-// };
-
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -126,19 +80,16 @@ exports.login = async (req, res) => {
     // Remove the password property from the response
     delete adminUser.password;
 
-    res.status(200).json({
+    return res.status(200).json({
       status: true,
       message: "Admin logged in successfully",
       data: adminUser,
       token,
     });
-
   } catch (error) {
     console.error(error);
-    
-    res.status(500).json({
+    return res.status(500).json({
       status: false,
-      data : error,
       message: "An error occurred during login",
     });
   }
