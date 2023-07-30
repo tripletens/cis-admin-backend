1693630236ee0a0a9218485d2658a3eaf38d5144
Kalu Chinonso <tripletens.kc@gmail.com>
2023-07-30 16:34:03 +0100

    added the live_uri for mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.LOCAL_URI);
+    await mongoose.connect(process.env.LIVE_URI);
     console.log('Connected to database!');
   } catch (error) {
     console.error('Connection to database failed:', error.message);
